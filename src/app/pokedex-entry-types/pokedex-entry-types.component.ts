663c5de36ae90d8ea2a5b19edaa587447b386bc0
Cory Sandlin <corybsa@gmail.com>
2021-08-23 07:31:34 -0400

    finished type weaknesses

--- src/app/pokedex-entry-types/pokedex-entry-types.component.ts
+++ src/app/pokedex-entry-types/pokedex-entry-types.component.ts
@@ -21,6 +21,8 @@ export class PokedexEntryTypesComponent implements OnInit, OnChanges {
   halfDamageFrom: string[] = [];
   normalDamageFrom: string[] = [];
   doubleDamageFrom: string[] = [];
+  quadDamageFrom: string[] = [];
+  noDamageFrom: string[] = [];
 
   constructor(
     private typesService: PokemonTypesService
@@ -58,21 +60,29 @@ export class PokedexEntryTypesComponent implements OnInit, OnChanges {
   }
 
   calculateWeaknesses() {
-    // TODO: edge cases for ghost, dragon, etc...
-
     // pokemon has two types
     if(this.damageRelations.length === 2) {
       const pokemonType1 = this.pokemon.types[0].type.name;
       const pokemonType2 = this.pokemon.types[1].type.name;
 
-      let halfDamage = _.pluck(_.union(this.damageRelations[0].half_damage_from, this.damageRelations[1].half_damage_from), 'name');
-      let doubleDamage = _.pluck(_.union(this.damageRelations[0].double_damage_from, this.damageRelations[1].double_damage_from), 'name');
+      const halfDamage1 = _.pluck(this.damageRelations[0].half_damage_from, 'name');
+      const halfDamage2 = _.pluck(this.damageRelations[1].half_damage_from, 'name');
+      const doubleDamage1 = _.pluck(this.damageRelations[0].double_damage_from, 'name');
+      const doubleDamage2 = _.pluck(this.damageRelations[1].double_damage_from, 'name');
+      this.noDamageFrom = _.pluck(_.union(this.damageRelations[0].no_damage_from, this.damageRelations[1].no_damage_from), 'name');
+
+      const halfDamage = _.union(halfDamage1, halfDamage2);
+      const doubleDamage = _.union(doubleDamage1, doubleDamage2);
       
       this.halfDamageFrom = _.without(halfDamage, ...doubleDamage);
-      this.quarterDamageFrom = _.intersection(this.halfDamageFrom, [pokemonType1, pokemonType2]);
+      this.quarterDamageFrom = _.intersection(halfDamage1, halfDamage2);
       this.halfDamageFrom = _.difference(this.halfDamageFrom, this.quarterDamageFrom);
-      this.doubleDamageFrom = _.difference(doubleDamage, halfDamage);
-      this.normalDamageFrom = _.difference(PokemonTypes.Types, this.halfDamageFrom, this.doubleDamageFrom, this.quarterDamageFrom);
+      this.quadDamageFrom = _.intersection(doubleDamage1, doubleDamage2);
+      this.doubleDamageFrom = _.without(_.difference(doubleDamage, halfDamage), ...this.quadDamageFrom);
+      this.normalDamageFrom = _.without(
+        _.difference(PokemonTypes.Types, this.halfDamageFrom, this.doubleDamageFrom, this.quarterDamageFrom, this.quadDamageFrom),
+        ...this.noDamageFrom
+      );
     } else {
       this.halfDamageFrom = _.pluck(this.damageRelations[0].half_damage_from, 'name');
       this.doubleDamageFrom = _.pluck(this.damageRelations[0].double_damage_from, 'name');
