71ed66105c1f7bb45369764d6c53e2b23fbed09e
Cory Sandlin <corybsa@gmail.com>
2021-08-31 10:11:59 -0400

    dist folder for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'pokedex'
-  },
-  {
     path: 'pokedex',
     loadChildren: () => import('./pokedex/pokedex.module').then(m => m.PokdexModule)
   },
   {
     path: 'typedex',
     loadChildren: () => import('./typedex/typedex.module').then(m => m.TypedexModule)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'pokedex'
   }
 ];
 
